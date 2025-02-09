038be3284a7c47eb91d5ee8bb8514df23acd6005
Zhivko Shopov <zhishopov@gmail.com>
2025-02-09 14:32:52 +0000

    Add dynamic movie rendering on home page

--- src/movies.js
+++ src/movies.js
@@ -5,7 +5,7 @@ export default [
     genre: "Adventure",
     director: "Rob Marshal",
     year: 2023,
-    imageURL: "/img/jungle-cruise.jpeg",
+    imageURL: "/static/img/jungle-cruise.jpeg",
     rating: 8.9,
     description:
       "Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world.",
@@ -16,7 +16,7 @@ export default [
     genre: "Comedy",
     director: "Chris Columbus",
     year: 1990,
-    imageURL: "/img/home-alone.jpeg",
+    imageURL: "/static/img/home-alone.jpeg",
     rating: 10,
     description:
       "A 1990 Christmas comedy film about an eight-year-old boy, Kevin McCallister, who is accidentally left home alone by his family",
@@ -27,7 +27,7 @@ export default [
     genre: "Cartoon",
     director: "John Musker",
     year: 2015,
-    imageURL: "/img/the-little-mermaid.jpg",
+    imageURL: "/static/img/the-little-mermaid.jpg",
     rating: 6,
     description:
       "A fairy tale by Hans Christian Andersen about a mermaid who wants to be human.",
