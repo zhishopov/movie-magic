6ffb85b1612be48f8a7e4a078484257525684e2a
Zhivko Shopov <zhishopov@gmail.com>
2025-02-09 14:40:15 +0000

    Add movie controller

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -11,8 +11,4 @@ router.get("/about", (req, res) => {
   res.render("about");
 });
 
-router.get("/create", (req, res) => {
-  res.render("create");
-});
-
 export default router;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import homeController from "./controllers/home-controller.js";
+import movieController from "./controllers/movie-controller.js";
 
 const routes = Router();
 
 routes.use(homeController);
+routes.use(movieController);
 
 routes.get("*", (req, res) => {
   res.render("404");
