d45776f5513077317678a3c52e552272ff929859
Zhivko Shopov <zhishopov@gmail.com>
2025-02-10 00:20:25 +0000

    Add filter values to filter form

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -11,7 +11,7 @@ movieController.get("/search", (req, res) => {
   const filter = req.query;
 
   const movies = movieService.getAll(filter);
-  res.render("search", { movies });
+  res.render("search", { movies, filter });
 });
 
 movieController.get("/:movieId/details", (req, res) => {
