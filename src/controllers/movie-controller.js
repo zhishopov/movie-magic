a3e58e9fea616519d734b81fcd85e5316a777fe7
Zhivko Shopov <zhishopov@gmail.com>
2025-02-13 14:02:02 +0000

    Show cast in attach page

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import movieService from "../services/movie-service.js";
+import castService from "../services/cast-service.js";
 
 const movieController = Router();
 
@@ -32,7 +33,9 @@ movieController.get("/:movieId/details", async (req, res) => {
 movieController.get("/:movieId/attach-cast", async (req, res) => {
   const movieId = req.params.movieId;
   const movie = await movieService.getOne(movieId).lean();
-  res.render("movie/attach-cast", { movie });
+  const casts = await castService.getAll().lean();
+
+  res.render("movie/attach-cast", { movie, casts });
 });
 
 export default movieController;
--- src/services/cast-service.js
+++ src/services/cast-service.js
@@ -1,6 +1,10 @@
 import Cast from "../models/Cast.js";
 
 export default {
+  getAll() {
+    return Cast.find({});
+  },
+
   createCast(castData) {
     return Cast.create(castData);
   },
