bd533b946e3e1868e5f616cb109bfb82b3431f23
Zhivko Shopov <zhishopov@gmail.com>
2025-02-14 22:36:46 +0000

    Add bcrypt and jwt libraries

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -6,4 +6,10 @@ authController.get("/register", (req, res) => {
   res.render("auth/register");
 });
 
+authController.post("/register", (req, res) => {
+  const { email, password, repass } = req.body;
+
+  res.redirect("/");
+});
+
 export default authController;
