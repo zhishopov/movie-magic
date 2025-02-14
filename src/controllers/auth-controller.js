c776578b853070056bf612507d8bd70c668eee1e
Zhivko Shopov <zhishopov@gmail.com>
2025-02-14 22:27:38 +0000

    Update navigation

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -2,4 +2,8 @@ import { Router } from "express";
 
 const authController = Router();
 
+authController.get("/register", (req, res) => {
+  res.render("auth/register");
+});
+
 export default authController;
