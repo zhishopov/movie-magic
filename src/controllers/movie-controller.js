7fe241653715bee7641b6abfebd5d1cf8a84b426
Zhivko Shopov <zhishopov@gmail.com>
2025-02-15 10:17:11 +0000

    Show details buttons if user is creator

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -29,8 +29,9 @@ movieController.get("/:movieId/details", async (req, res) => {
 
   const movieId = req.params.movieId;
   const movie = await movieService.getOneWithCasts(movieId).lean();
+  const isCreator = movie.creator && movie.creator.toString() === req.user?.id;
 
-  res.render("movie/details", { movie });
+  res.render("movie/details", { movie, isCreator });
 });
 
 movieController.get("/:movieId/attach-cast", async (req, res) => {
