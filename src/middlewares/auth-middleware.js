6b2d1b9141e0bcb9881f6bde79f8703fd7543cb9
Zhivko Shopov <zhishopov@gmail.com>
2025-02-15 10:33:22 +0000

    Add dynamic navigation

--- src/middlewares/auth-middleware.js
+++ src/middlewares/auth-middleware.js
@@ -15,6 +15,7 @@ export const authMiddleware = (req, res, next) => {
 
     // Attach decoded token to request
     req.user = decodedToken;
+    res.locals.user = decodedToken;
 
     next();
   } catch (error) {
