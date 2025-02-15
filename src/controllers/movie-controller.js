1a685e4860c7abc7e50d1bb32823d5570ef52ebe
Zhivko Shopov <zhishopov@gmail.com>
2025-02-15 10:23:33 +0000

    Fix creator check

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -29,7 +29,7 @@ movieController.get("/:movieId/details", async (req, res) => {
 
   const movieId = req.params.movieId;
   const movie = await movieService.getOneWithCasts(movieId).lean();
-  const isCreator = movie.creator && movie.creator.toString() === req.user?.id;
+  const isCreator = movie.creator?.equals(req.user?.id);
 
   res.render("movie/details", { movie, isCreator });
 });
